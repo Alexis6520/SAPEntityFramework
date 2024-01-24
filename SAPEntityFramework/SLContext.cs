ea32d944f8be8bdf56429d70b00a0a44710c43e4
Alexis <alexis.ortega@anconaautopartes.com>
2024-01-24 10:09:13 -0500

    Se deshabilita la paginación por defecto

--- SAPEntityFramework/SLContext.cs
+++ SAPEntityFramework/SLContext.cs
@@ -24,6 +24,7 @@ namespace SAPSLFramework
                 BaseAddress = new Uri(_options.Url)
             };
 
+            HttpClient.DefaultRequestHeaders.Add("B1S-PageSize", "0");
             InitializeSets();
         }
 
